50ce7b6d3445ea14283385b697592d053026f725
Paul Keogh <Paul.Keogh@synchronoss.com>
2022-01-02 15:27:43 +0000

    Test updates

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import './views/my_home_page.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
